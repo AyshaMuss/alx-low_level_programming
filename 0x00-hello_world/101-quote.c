b5d552bbe5e5e6c66863d850a5bbbcef063b88c7
AyshaMuss <Ayshamuss@gmail.com>
2021-08-26 19:06:40 +0300

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@ int main(void)
 { 
     char phrase[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+    fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
      , sizeof(phrase));
     return (1);
 }
