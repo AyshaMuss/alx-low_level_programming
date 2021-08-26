19041f21bef9bddf320c35b1a6c121e1a3d924c5
AyshaMuss <Ayshamuss@gmail.com>
2021-08-26 14:57:26 +0300

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+   char string[40];
+   strcpy(str, "Programming is like building a multilingual puzzle\n");
+   puts(string);
+   return 0;
+}
