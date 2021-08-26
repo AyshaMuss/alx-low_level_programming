a0fff7784cf5cc86ef203e17bf2f5520f23cf0aa
AyshaMuss <Ayshamuss@gmail.com>
2021-08-26 17:55:50 +0300

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,8 @@ int main(void)
 {
 printf("Size of int: %d byte(s)\n", sizeof(int));
 printf("Size of float: %d byte(s)\n", sizeof(float));
-printf("Size of double: %d byte(s)\n", sizeof(double));
+printf("Size of long: %d byte(s)\n", sizeof(long));
 printf("Size of char: %d byte(s)\n", sizeof(char));
+printf("size of long double: %d byte(s)\n", sizeof(long double));
 return (0);
 }
