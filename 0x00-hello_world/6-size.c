b39cec588042ed7dc1b05e7a5561158fc2089046
AyshaMuss <Ayshamuss@gmail.com>
2021-08-26 17:47:27 +0300

    hello world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int int;
+float float;
+double double;
+char char;
+// sizeof evaluates the size of a variable
+printf("Size of int: %d bytes\n", sizeof(int));
+printf("Size of float: %d bytes\n", sizeof(float));
+printf("Size of double: %d bytes\n", sizeof(double));
+printf("Size of char: %d byte\n", sizeof(char));
+return (0);
+}
