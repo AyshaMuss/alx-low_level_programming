5767ed2076bcc2c4638e03e8666d9513d58cc5da
AyshaMuss <Ayshamuss@gmail.com>
2021-08-27 21:02:31 +0300

    variables if else while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,8 +11,8 @@ for (i1 = 0; i1 < 8; i1++)
 {
 for (i2 = 1; i2 < 10; i2++)
 {
-putchar((i1 % 10) + '1');
-putchar((i2 % 10) + '0');
+putchar((i1 % 10) + '0');
+putchar((i2 % 10) + '1');
 if (i1 == 9 && i2 == 9)
 continue;
 else
