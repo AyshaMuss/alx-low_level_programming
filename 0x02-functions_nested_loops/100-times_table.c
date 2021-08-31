fda3b65f907d983148f47b32ce74148c5fd3e7da
AyshaMuss <Ayshamuss@gmail.com>
2021-08-31 21:16:00 +0300

    functions nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,15 +8,39 @@
  */
 void print_times_table(int n)
 {
-    for (n = 0; n <=15; n++)
-    print_times_table(2);
-    _putchar('\n');
-    print_times_table(3);
-    _putchar('\n');
-    print_times_table(5);
-    _putchar('\n');
-    print_times_table(98);
-    _putchar('\n');
-    print_times_table(12);  
-    return (0);
+
+int a, b;
+int mult;
+
+for (a = 0; a <= 15; a++)
+{
+for (b = 0; b <= 15; b++)
+{
+mult = a * b;
+if (mult >= 10)
+{
+_putchar(' ');
+_putchar((mult / 10) + '0');
+_putchar((mult % 10) + '0');
+if (b != 15)
+{
+_putchar(',');
+}
+}
+else
+{
+if (b != 0)
+{
+_putchar(' ');
+_putchar(' ');
+}
+_putchar(mult + '0');
+if (b != 15)
+{
+_putchar(',');
+}
+}
+}
+_putchar('\n');
+}
 }
