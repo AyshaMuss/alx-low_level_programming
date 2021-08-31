aa7d8e866f98716c8c4acc3611a236ee9d32f2e5
AyshaMuss <Ayshamuss@gmail.com>
2021-08-31 18:23:38 +0300

    functions nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet -Entry point
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char c;
+int count;
+for (count = 'a'; count < 10; count++)
+{
+for (c = 'a'; a <= 'z'; c++)
+_putchar(c)
+_putchar ('\n');
+}
