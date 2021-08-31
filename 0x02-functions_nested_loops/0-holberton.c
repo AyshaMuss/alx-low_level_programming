bd7d2ca54057b15446642d24ac0db66fff006c77
AyshaMuss <Ayshamuss@gmail.com>
2021-08-31 16:37:37 +0300

    functions nested loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * main(void) to run the code and return 0
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int _putchar(char c);
 int main(void)
