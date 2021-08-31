704799a07300dee68a70163d94782b28d6f7aab4
AyshaMuss <Ayshamuss@gmail.com>
2021-08-31 21:08:22 +0300

    functions nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,6 +9,8 @@
 void print_times_table(int n)
 {
     for (n = 0; n <=15; n++)
+    print_times_table(2);
+    _putchar('\n');
     print_times_table(3);
     _putchar('\n');
     print_times_table(5);
