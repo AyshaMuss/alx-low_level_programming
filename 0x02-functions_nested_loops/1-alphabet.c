a607d543be40e38b861fd93200828c3eba6378a7
AyshaMuss <Ayshamuss@gmail.com>
2021-08-31 18:04:00 +0300

    functions nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
  * main - Entry point
- * @c: The character to print
- *
- * void print_alphabet(void): the character to be printed
+ * @print_alphabet(void): the character to be printed
+ * 
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
