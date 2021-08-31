20072cd6a967f5d8c31991033956b1a9c7c7b0c6
AyshaMuss <Ayshamuss@gmail.com>
2021-08-31 16:48:01 +0300

    functions nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * print_alpahbet(void): The character to print
+ *
+ * Return: Always 0 (Success)
+ */
+ void print_alphabet(void);
+ int main(void)
+ {
+ char c;
+ for (c = 'a' c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n')
+return (0);
+ }
+ 
