b1b07b87ee6fc3618a5bb046d790f8016b3b6e8d
AyshaMuss <Ayshamuss@gmail.com>
2021-08-31 15:53:58 +0300

    functions nested loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,6 +1,5 @@
 #include <unistd.h>
 #include "main.h"
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -9,7 +8,7 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
-int main(int _putchar)
+int main(void)
 {
 _putchar('_');
 _putchar('p');
