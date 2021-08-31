2ef3beb9d558c671db5eb8f288dc45b913b229c3
AyshaMuss <Ayshamuss@gmail.com>
2021-08-31 21:02:09 +0300

    functions nested loops

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * void print_times_table - Entry poit.
+ *@n - variable character
+ *
+ * Return: Always 0.
+ */
+void print_times_table(int n)
+{
+    for (n = 0; n <=15; n++)
+    print_times_table(3);
+    _putchar('\n');
+    print_times_table(5);
+    _putchar('\n');
+    print_times_table(98);
+    _putchar('\n');
+    print_times_table(12);  
+    return (0);
+}
