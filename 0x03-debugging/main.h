f72722c599b261da76fcf80148cc7681773b6f4d
AyshaMuss <Ayshamuss@gmail.com>
2021-09-02 19:47:15 +0300

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stdio.h>
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -13,7 +14,7 @@ void times_table(void);
 void print_times_table(int n);
 int add(int a, int b);
 void print_to_98(int n);
-int largest_number(int a, int b, int c)
-void print_remaining_days(int month, int day, int year)
-int convert_day(int month, int day)
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 #endif
