2e28c8c3b042ac72b4e40d445db0c8765fc72764
AyshaMuss <Ayshamuss@gmail.com>
2021-09-02 20:36:32 +0300

    more functions nested loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_number(int n)
+{
+
+   if (n < 0) {
+       _putchar('-');
+       n = -n;
+   }
+
+   if (n == 0)
+      _putchar('0');
+
+   if (n/10)
+      printnumber(n/10);
+
+   _putchar(n%10 + '0');
+}
+
+int main(int argc, char** argv)
+{
+   int n = atoi(argv[1]);
+   printnumber(n);
+   _putchar('\n');
+}
