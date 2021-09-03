53bb72e683c2c954ee76035c72c19ad572354619
AyshaMuss <Ayshamuss@gmail.com>
2021-09-03 12:06:33 +0300

    more functions nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -16,7 +16,7 @@ int _putchar(char c);
 int my_put_nbr(int n);
 void print_int_r(int x, int neg);
 void print_int(int x);
-void print_number(int n, int neg, int x);
 int main(void);
 _putchar();
+void print_number(int n);
 #endif
