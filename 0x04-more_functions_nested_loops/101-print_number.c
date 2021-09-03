10e850200bfbaa884989a5251a8b8c8f35f454cb
AyshaMuss <Ayshamuss@gmail.com>
2021-09-03 10:26:39 +0300

    more functions nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,20 +5,17 @@
  *
  * Return: Always 0 (Success)
  */
-void print_number(int n)
+void print_int_r (int x, int neg)
 {
-        if (n < 0)
-        {
-                my_putchar(0x2D);
-                n = -n;
-        }
+    int y = x/10;
+    int d = x%10;
+    if (y) print_int_r(y, neg);
+    putchar('0' + (neg ? -d : d));
+}
 
-        if (n > 9)
-        {
-                my_put_nbr(n/10);
-        }
-
-        my_putchar((n%10) + '0');
-
-        return 0;
+void print_int (int x) {
+    int neg = x < 0;
+    if (neg) putchar('-');
+    print_int_r(x, neg);
+    putchar('\n');
 }
