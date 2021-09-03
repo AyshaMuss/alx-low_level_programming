104aea605cd505478d955ebd8add2c1ba8623d9f
AyshaMuss <Ayshamuss@gmail.com>
2021-09-03 10:50:01 +0300

    more functions nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,27 +6,27 @@
  * Return: Always 0 (Success)
  */
 void print_number(int n)
-{
-    // If number is smaller than 0, put a - sign
-    // and change number to positive
+{ 
+    // If number is smaller than 0, put a - sign 
+    // and change number to positive 
     if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
+    { 
+      putchar('-'); 
+      n = -n; 
+    } 
   
-    // Remove the last digit and recur
-    if (n/10)
-      _putchar('n/10');
+    // Remove the last digit and recur 
+    if (n/10) 
+   print(n/10); 
   
-    // Print the last digit
-    _putchar(n%10 + '0');
-}
+    // Print the last digit 
+   _putchar(n%10 + '0'); 
+} 
   
-// Driver program to test abvoe function
-int main()
-{
-   int n = 12045;
-   _putchar('n');
-   return (0);
+// Driver program to test abvoe function 
+int main() 
+{ 
+   int n = 12045; 
+   print(n); 
+   return 0; 
 }
