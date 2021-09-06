37a64ad0e47ef86fbf55377aa1002659667fbfaf
AyshaMuss <Ayshamuss@gmail.com>
2021-09-06 17:11:17 +0300

    more functions nested loops

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,17 +9,13 @@
  * Return: Always 0.
  */
 void print_rev(char *s)
+{   
+int i, len, temp;  
+len = strlen(s); 
+for (i = 0; i < len/2; i++)  
 {  
-    // declare variable  
-    int i, len, temp;  
-    len = strlen(s); // use strlen() to get the length of str string  
-      
-    // use for loop to iterate the string   
-    for (i = 0; i < len/2; i++)  
-    {  
-        // temp variable use to temporary hold the string  
-        temp = s[i];  
-        s[i] = s[len - i - 1];  
-        s[len - i - 1] = temp;  
-    }  
+temp = s[i];  
+s[i] = s[len - i - 1];  
+s[len - i - 1] = temp;  
+}  
 }  
