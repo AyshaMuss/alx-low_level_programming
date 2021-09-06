81dcb901d548b3867dc935ad7ee4ea795eb86bcf
AyshaMuss <Ayshamuss@gmail.com>
2021-09-06 15:01:56 +0300

    pointers arrays strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - swaps integers with pointers.
+ *
+ * @s: is pointer to an char.
+ * char: is a data type.
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+int i;
+while (*(s + i)) != '\0')
+{
+i++;
+}
+return (i);
+}
