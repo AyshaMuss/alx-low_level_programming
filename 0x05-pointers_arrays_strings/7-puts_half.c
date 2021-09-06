bb606f06feb7056ffc335bbec3a6d034b4993484
AyshaMuss <Ayshamuss@gmail.com>
2021-09-06 18:33:26 +0300

    pointers array strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,7 +11,15 @@
 void puts_half(char *str)
 {
 int i, j;
-i = *str;
-_putchar(*(str / 2));
+
+	while (*(str + i) != '\0')
+	i++;
+
+	if (i % 2 != 0)
+		for (j = ((i + 1) / 2); *(str + j) != '\0'; j++)
+			_putchar(*(str + j));
+	else if (i % 2 == 0)
+		for (j = (i / 2); *(str + j) != '\0'; j++)
+			_putchar(*(str + j));
 _putchar('\n');
 }
