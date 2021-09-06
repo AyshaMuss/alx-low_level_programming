0bd7640f634c7dc5df561e34b52dfd70b65045b3
AyshaMuss <Ayshamuss@gmail.com>
2021-09-06 14:12:01 +0300

    pointers arrays strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * reset_to_98 - resets the value.  
+ * reset_to_98 - checks the code  
  *
- * n: is the character to be printed.
+ * @n: is pointer to an int.
+ * 
+ * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
-    n = 98;
+n = 98;
 }
