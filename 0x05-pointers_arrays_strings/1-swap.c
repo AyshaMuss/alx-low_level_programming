fde480677c65400e5af3492076510bc6e8894edf
AyshaMuss <Ayshamuss@gmail.com>
2021-09-06 14:34:54 +0300

    pointers arrays strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - checks the code
+ *
+ * @a: is pointer to an int.
+ * @b: is pointer to an int.
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b);
+{
+  int a = 98;
+  int b = 42;
+  printf("a is %d, b is %d\n", a, b);
+  swap(&a, &b);
+  printf("a is %d, b is %d\n", a, b);
+  return (0);
+  }
