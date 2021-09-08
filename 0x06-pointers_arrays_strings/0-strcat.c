285d46aa3739a14ab6d932ab6e1b5ac327a13e03
AyshaMuss <Ayshamuss@gmail.com>
2021-09-08 12:03:11 +0300

    0x06 pointers array strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,21 +2,20 @@
 #include <stdio.h>
 
 /**
- * _strcat - This function concatenates two strings.
+ *_strcat - This function concatenates two strings.
  * 
- * @dest: is a pointer to a char.
- * @src: is a pointer to a char.
+ *@dest: is a pointer to a char.
+ *@src: is a pointer to a char.
  * 
- * Return: Always 0.
- * /
+ *Return: Always 0.
+ */
  char *_strcat(char *dest, char *src)
- {
+{
 int i = _strlen(dest), j;
 for (j = 0; src[j] != '\0'; j++)
 {
-    dest[i + j] = src[j];
+dest[i + j] = src[j];
 }
 dest[i + j] = '\0';
 return (dest);
- }
- 
+}
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strncat - This function concatenates two strings.
+ *
+ *@dest: is a pointer to a char.
+ *@src: is a pointer to a char.
+ *@n: is an int.
+ * 
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = _strlen(dest), j;
+for (j = 0; j < n && src[j] != '\0'; j++)
+{
+    dest[i + j] = src[j];
+}
+dest[i + j] = '\0';
+return (dest);
+}
+/**
+* _strlen - Measures the lenght of a string.
+* 
+* @s: is a pointer to a char.
+*
+*  Return: Always 0.
+*/
+int _strlen(char *s)
+{
+int i = 0;
+while (*(s + i) != '\0')
+{
+i++;
+}
+return (i);
+}  
