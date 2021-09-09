397e05cb24ba01accdc6b1cc06adc317154884d8
AyshaMuss <Ayshamuss@gmail.com>
2021-09-09 17:27:59 +0300

    0x06-pointers array strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,32 +1,48 @@
 #include "main.h"
 
 /**
- * cap_string - Converts to uppercase any char after a special char.
- *
- * @s: is a pointer to a char.
- *
+ * print_number - prints an integer.
+ * 
+ * @n: number tested
  * Return: Always 0.
  */
-
-char *cap_string(char *s)
+void print_number(int n)
 {
-int i = 0, flag = 0;
+int i, j, digit, digits, power;
+unsigned int temp, numchar, number;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == '\t' || s[i] == '\n' || s[i] == ' ' || s[i] == ',' || s[i] == ';'
-		|| s[i] == '.' || s[i] == '!' || s[i] == '"' || s[i] == '(' || s[i] == ')'
-		|| s[i] == '{' || s[i] == '}')
-			flag = 1;
-		else if ((s[i] >= 97 && s[i] <= 122 && flag == 1)
-		|| (s[0] >= 97 && s[0] <= 122))
-		{
-			s[i] -= 32;
-			flag = 0;
-		}
-		else
-		flag = 0;
-	i++;
-	}
-return (s);
+digit = 0;
+if (n < 0)
+{
+_putchar('-');
+temp = -n;
+}
+else
+{
+temp = n;
+}
+
+number = temp;
+
+while (number >= 10)
+{
+number = number / 10;
+digit++;
+}
+digits = digit + 1;
+power = 1;
+i = 1;
+
+while (i < digits)
+{
+power = power * 10;
+i++;
+}
+j = power;
+while (j >= 1)
+{
+numchar = (temp / j) % 10;
+_putchar(numchar + '0');
+j = j / 10;
+}
 }
