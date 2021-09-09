31be8ba78bb8e02f26a3958a53ad3de6ca910512
AyshaMuss <Ayshamuss@gmail.com>
2021-09-09 16:24:12 +0300

    0x06-pointers array strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,37 +1,46 @@
 #include "main.h"
 
 int _strlen(char *s);
+
 /**
-*_strcat - This function concatenates two strings.
-* 
-*@dest: is a pointer to a char.
-*@src: is a pointer to a char.
-* 
-*Return: Always 0.
-*/
+ * _strcat - This function concatenates two strings.
+ *
+ * @dest: is a pointer to a char.
+ * @src: is a pointer to a char.
+ *
+ * Return: Always 0.
+ */
+
 char *_strcat(char *dest, char *src)
 {
 int i = _strlen(dest), j;
 
-for (j = 0; src[j] != '\0'; i++)
-{
-dest[j + i] = src[j];
-}
-dest[j + i] = '\0';
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+
+	dest[i + j] = '\0';
+
 return (dest);
 }
+
 /**
- * _strlen - this function measures a string length.
- * @s: is a pointer to a char.
- * 
+ * _strlen - this function measures a string leght.
+ *
+ * @s: is a pointer to a char
+ *
  * Return: Always 0
  */
+
 int _strlen(char *s)
 {
 int i = 0;
-while (*(s + 1) != '\0')
-{
-i++;
-}
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
 return (i);
 }
