d8773ea0be0995a979ae18dd8930e5d783bcd3f5
AyshaMuss <Ayshamuss@gmail.com>
2021-09-09 14:22:44 +0300

    0x06-pointers array strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,37 +1,49 @@
 #include "main.h"
 
+int _strlen(char *s);
+
 /**
- * _strncat - This function concatenates two strings.
+ * _strncat - Concatenates two strings.
  *
- *@dest: is a pointer to a char.
- *@src: is a pointer to a char.
- *@n: is an int.
- * 
+ * @dest: is a pointer to a string.
+ *
+ * @src: is a pointre to a string.
+ *
+ * @n: is an int
+ *
  * Return: Always 0.
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
 int i = _strlen(dest), j;
-for (j = 0; j < n && src[j] != '\0'; j++)
-{
-    dest[i + j] = src[j];
-}
-dest[i + j] = '\0';
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+
+	dest[i + j] = '\0';
+
 return (dest);
 }
+
 /**
-* _strlen - Measures the lenght of a string.
-* 
-* @s: is a pointer to a char.
-*
-*  Return: Always 0.
-*/
+ *_strlen - Measures the lenght of a string.
+ *
+ * @s: is a pointer to a char.
+ *
+ * Return: Always 0.
+ */
+
 int _strlen(char *s)
 {
 int i = 0;
-while (*(s + i) != '\0')
-{
-i++;
-}
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
 return (i);
-}  
+}
