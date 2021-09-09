f843c25decad278afd2d96b8b6591af977a0ad55
AyshaMuss <Ayshamuss@gmail.com>
2021-09-09 17:20:28 +0300

    0x06-pointers array strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,47 +1,32 @@
 #include "main.h"
+
 /**
- * print_number - prints an integer.
- * 
- * @n: number tested
+ * cap_string - Converts to uppercase any char after a special char.
+ *
+ * @s: is a pointer to a char.
+ *
  * Return: Always 0.
  */
-void print_number(int n)
-{
-int i, j, digit, digits, power;
-unsigned int temp, numchar, number;
 
-digit = 0;
-if (n < 0)
+char *cap_string(char *s)
 {
-_putchar('-');
-temp = -n;
-}
-else
-{
-temp = n;
-}
+int i = 0, flag = 0;
 
-number = temp;
-
-while (number >= 10)
-{
-number = number / 10;
-digit++;
-}
-digits = digit + 1;
-power = 1;
-i = 1;
-
-while (i < digits)
-{
-power = power * 10;
-i++;
-}
-j = power;
-while (j >= 1)
-{
-numchar = (temp / j) % 10;
-_putchar(numchar + '0');
-j = j / 10;
-}
+	while (s[i] != '\0')
+	{
+		if (s[i] == '\t' || s[i] == '\n' || s[i] == ' ' || s[i] == ',' || s[i] == ';'
+		|| s[i] == '.' || s[i] == '!' || s[i] == '"' || s[i] == '(' || s[i] == ')'
+		|| s[i] == '{' || s[i] == '}')
+			flag = 1;
+		else if ((s[i] >= 97 && s[i] <= 122 && flag == 1)
+		|| (s[0] >= 97 && s[0] <= 122))
+		{
+			s[i] -= 32;
+			flag = 0;
+		}
+		else
+		flag = 0;
+	i++;
+	}
+return (s);
 }
