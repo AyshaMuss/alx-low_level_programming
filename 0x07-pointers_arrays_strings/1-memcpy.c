f98d96d5fc04cea5c6956c23afc27bcd62cec91c
AyshaMuss <Ayshamuss@gmail.com>
2021-09-13 14:36:10 +0300

    0x07-pointers array strings

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _memcpy - This function fills memmory with a constant byte
+ * _memcpy - This function fills memory with a constant byte
  * 
  * @dest: is a pointer to a char
  * @src: is a pointer to a char
  * @n: is an unsigned int
  * 
- * Return: Return the transformed pointer
+ * Return: Returns the transformed pointer
  * 
 **/
 
-char *_memset(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
 
-for (i =0; i < n; i++)
+for (i = 0; i < n; i++)
 dest[i] = src[i];
 
 return (dest);
