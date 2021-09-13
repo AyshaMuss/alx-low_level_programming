5282e1b9e3efb775e136b532a35f692191d4295a
AyshaMuss <Ayshamuss@gmail.com>
2021-09-13 15:25:04 +0300

    0x07-pointers array strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * _strstr - locates a substring/
- * 
+ * _strstr - locates a substring
+ *
  * @haystack: is a pointer to a char
  * @needle: is a pointer to a char
- * 
+ *
  * Return: Return the transformed pointer
- * 
- **/
+ *
+**/
 
 char *_strstr(char *haystack, char *needle)
 {
-    int i;
-    for (i = 0; haystack[i]; i++)
-    if (haystack[i] == needle[0])
-    return (haystack + i);
-    
-    if (haystack[i] == needle[0])
-    return (haystack + i);
+int i;
+for (i = 0; haystack[i]; i++)
+if (haystack[i] == needle[0])
+return (haystack + i);
 
-    return (0);
+if (haystack[i] == needle[0])
+return (haystack + i);
+
+return (0);
 }
