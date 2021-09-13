3ee474691274d65fed1751a68e12aceb448d7d7d
AyshaMuss <Ayshamuss@gmail.com>
2021-09-13 15:39:28 +0300

    0x07-pointers array strings

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,26 +2,26 @@
 
 /**
  * print_diagsums - prints the sum of the two diagonals.
- * 
+ *
  * @a: is a pointer to an int.
  * @size: is a pointer to an int.
- * 
+ *
  * Return: Returns the transformed pointer
- * 
- **/
+ *
+**/
 
 void print_diagsums(int *a, int size)
 {
-    int i, j, sum = 0;
-    
-    for (i = 0; i < size; i++)
-    sum += a[(size *i) + i];
+int i, j, sum = 0;
 
-    printf("%d, ", sum);
-    sum = 0;
+for (i = 0; i < size; i++)
+sum += a[(size *i) + i];
 
-    for (j = 0; j < size; j++)
-    sum += a[(size *(j + 1))- (j + 1)];
+printf("%d, ", sum);
+sum = 0;
 
-    printf("%d\n", sum);
+for (j = 0; j < size; j++)
+sum += a[(size *(j + 1))- (j + 1)];
+
+printf("%d\n", sum);
 }
