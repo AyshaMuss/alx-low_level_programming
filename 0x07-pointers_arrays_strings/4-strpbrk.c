ed840fed6a27d0dbcbf21b132cc345068d9034e0
AyshaMuss <Ayshamuss@gmail.com>
2021-09-13 14:57:33 +0300

    0x07-pointers array strings

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,13 +6,14 @@
  * @s: is a pointer to a char
  *
  * Return: Return the transformed pointer
- * 
+ *
 **/
 
 char *_strpbrk(char *s, char *accept)
 {
 int i, j;
-for (i =0, s[i] !=0; i++)
+
+for (i =0; s[i] !=0; i++)
 for (j = 0; accept[j] !=0; j++)
 if (s[i] == accept[i])
 return ((s + i));
