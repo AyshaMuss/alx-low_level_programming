3648a28e5fc87ef4ce2e6b45013273a361496a2b
AyshaMuss <Ayshamuss@gmail.com>
2021-09-13 14:53:53 +0300

    0x07-pointers array strings

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,20 +2,20 @@
 
 /**
  * _strpbrk - searches a string for any of a set of bytes.
- * 
+ *
  * @s: is a pointer to a char
- * 
+ *
  * Return: Return the transformed pointer
  * 
- **/
+**/
 
 char *_strpbrk(char *s, char *accept)
 {
-    int i, j;
-    for (i =0, s[i] !=0; i++)
-    for (j = 0; accept[j] !=0; j++)
-    if (s[i] == accept[i])
-    return ((s + i));
+int i, j;
+for (i =0, s[i] !=0; i++)
+for (j = 0; accept[j] !=0; j++)
+if (s[i] == accept[i])
+return ((s + i));
 
-    return (0);
+return (0);
 }
