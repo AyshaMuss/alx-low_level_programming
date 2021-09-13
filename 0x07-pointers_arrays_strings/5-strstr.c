fb80eedc2bda4d4fa34870de2fe4122588a7fc8e
AyshaMuss <Ayshamuss@gmail.com>
2021-09-13 15:51:11 +0300

    0x07-pointers array strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,10 +13,14 @@
 char *_strstr(char *haystack, char *needle)
 {
 int i;
+
 for (i = 0; haystack[i]; i++)
 if (haystack[i] == needle[0])
 return (haystack + i);
+
 if (haystack[i] == needle[0])
 return (haystack + i);
+
 return (0);
+
 }
