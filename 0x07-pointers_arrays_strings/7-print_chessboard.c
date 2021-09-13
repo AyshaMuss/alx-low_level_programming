8dc6321bd98c2c2225907ada172b6be2d237a701
AyshaMuss <Ayshamuss@gmail.com>
2021-09-13 15:33:09 +0300

    0x07-pointers array strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,20 +2,20 @@
 
 /**
  * print_chessboard - function that prints the chessboard.
- * 
+ *
  * @a: is a pointer to an array of chars
- * 
+ *
  * Return: Returns the transformed pointer
- * 
- **/
+ *
+**/
 
 void print_chessboard(char (*a)[8])
 {
-    int i, j;
-    
-    for (i = 0; i < 8; i++)
-    {
-        for (j = 0; j < 8; j++)
-        _putchar('\n');
-    }
+int i, j;
+
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+_putchar('\n');
+}
 }
