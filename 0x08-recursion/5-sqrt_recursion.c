077a7055b1c66b921df43e057a23b2806aa66e30
AyshaMuss <Ayshamuss@gmail.com>
2021-09-15 17:18:38 +0300

    0x08-recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _sprt_recursion - is a function that returns the natural square root of a number.
+ *
+ * @n: is an int to the function.
+ *
+ * Return: Return the transformed pointer
+ *
+**/
+
+int _sqrt_recursion(int n)
+{
+int i, result = 1;
+if (n == 0 || n == 1)
+{
+return (n);
+}
+while (result <= x)
+{
+i++;
+result = i * i;
+}
+
+return (i - 1);
+}
