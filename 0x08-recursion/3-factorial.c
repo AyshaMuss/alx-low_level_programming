2ab43fed90516f9cb9bd32e090c352984bc3182d
AyshaMuss <Ayshamuss@gmail.com>
2021-09-15 15:44:52 +0300

    0x08-recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,12 +11,16 @@
 
 int facrorial(int n)
 {
-if (n == 0)
+if (n > 0)
+{
+return (n * factorial(n - 1));
+}
+else if (n == 0)
 {
 return (1);
 }
 else if (n < 0)
 {
-return (n * factorial(n - 1));
+return (-1);
 }
 }
