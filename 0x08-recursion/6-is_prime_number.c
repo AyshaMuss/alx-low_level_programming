cf17a36087fda456d0ee758ba2818b3e0b9653b1
AyshaMuss <Ayshamuss@gmail.com>
2021-09-15 18:15:40 +0300

    0x08-recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ *
+ * @n: is a pointer to an int.
+ *
+ * Return: Returns the result.
+ *
+**/
+
+int is_prime_number(int n)
+{
+int num, p = 0;
+for (n = 1; n <= num; n++)
+{
+if (num % i == 0)
+{
+p++
+}
+}
+if (p == 2)
+{
+return (num);
+}
+else
+{
+return (0);
+}
+}
