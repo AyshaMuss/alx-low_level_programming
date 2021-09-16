9db48918942ccdfa59760b8d48b30823fe364309
AyshaMuss <Ayshamuss@gmail.com>
2021-09-16 12:39:07 +0300

    0x08-recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,19 +13,18 @@
 
 int wildcmp(char *s1, char *s2)
 {
-while( ( *s1 != '\0' && *s2 != '\0' ) && *s1 == *s2 )
+if (*s1 < *s2)
 {
-s1++;
-s2++;
+return -1;
 }
-
-if(*s1 == *s2)
+else if (*s1 > *s2)
+{
+return +1;
+}
+else if (*s1 == '\0')
 {
 return 0;
 }
-
 else
-{
-return *s1 - *s2;
-}
+return wildcmp(s1 + 1, s2 + 1);
 }
