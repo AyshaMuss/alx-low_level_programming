f162fea5377ca9ef569b0855c46256ddf115324e
AyshaMuss <Ayshamuss@gmail.com>
2021-09-18 12:01:35 +0300

    0x09-static_libraries

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _atoi - Function that converts char into int
+ *
+ * @s: pointer to a char
+ *
+ * Return: Returns the result
+**/
+
+int _atoi(char *s)
+{
+(void)*s;
+return (0);
+}
