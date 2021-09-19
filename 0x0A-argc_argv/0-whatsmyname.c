afe5a57b7328aefba0a5ba33291daf3a7a2b2177
AyshaMuss <ayshamuss@gmail.com>
2021-09-19 11:47:07 +0300

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - is a function that prints the name.
+ *@argc: is an argument counter for char.
+ *@argv: is an agument value for char.
+ *Return: Returns always success.
+**/
+
+int main(int argc, char *argv[])
+{
+	printf("program's name is: %s\n", argv[0]);
+	return (0);
+}
