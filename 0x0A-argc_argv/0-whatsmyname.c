fc627ba543730be884ccd0b25617a7954257790f
AyshaMuss <ayshamuss@gmail.com>
2021-09-19 12:03:34 +0300

    0x0A-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,11 +9,9 @@
 
 int main(int argc, char *argv[])
 {
-int i;
-printf("%d\n", argc);
-for (i = 0; i < argc - 1; i++)
-{
-printf("%S", argv[i]);
-}
-	return (0);
+int i = argc - argc;
+
+printf("%s\n", argv[i]);
+
+return (0);
 }
