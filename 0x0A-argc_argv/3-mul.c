1f50536a5685cbfad8691db4f3281f5f7913d6da
AyshaMuss <ayshamuss@gmail.com>
2021-09-19 15:51:57 +0300

    0x0A argc argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - prints the function.
+ * @num1: is int number.
+ * @num2: is an int number.
+ * @product: is an int number product.
+ *
+ * Return: Returns always success.
+**/
+
+int main(int argc, char *argv[])
+{
+int i = 0;
+if (i == 3)
+{
+  i +=(atoi(argv[(argc - argc)] + 1) * atoi(argv[(argc - argc)] + 2));
+printf("%d\n", i);
+return (0);
+}
+else
+{printf("Error\n");
+return (1);
+}
+}
