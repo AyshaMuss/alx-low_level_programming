831be89920a4a739351befbc699b29d39cb89f2e
AyshaMuss <ayshamuss@gmail.com>
2021-09-21 16:25:49 +0300

    0x0B malloc free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,28 +7,24 @@
  * @size: is the size of unsigned int array
  * @c: is a character for char.
  *
- * Return: Returns the array.
+ * Return: Returns to the array.
 **/
 
 char *create_array(unsigned int size, char c)
 {
+char *array = NULL;
 unsigned int i;
-char *ar = malloc(size * sizeof(c));
 
 if (size == 0)
+return (NULL);
+if (size != 0)
 {
-return (Null);
-}
-if (ar != Null)
+array = (char *)malloc(size * sizeof(char));
+if (array != NULL)
 {
 for (i = 0; i < size; i++)
-{
-ar[i] = c;
-return (ar);
+array[i] = c;
 }
 }
-else
-{
-return (Null);
-}
+return (array);
 }
