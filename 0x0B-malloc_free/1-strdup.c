23284b1dabdb80184b97d56d7f99a407f720d631
AyshaMuss <ayshamuss@gmail.com>
2021-09-21 17:26:56 +0300

    0x0B malloc free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,38 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include <string.h>
 
+/**
+ * _strlen - swaps integers with pointers
+ * @s: is a pointer to a char.
+ *
+ * Return: always success.
+**/
+int _strlen(char *s)
+{
+int i = 0;
+
+while (*(s + 1) != '\0')
+{
+i++;
+}
+return (i);
+}
 /**
  * _strdup - Entry point
  *@str: string we need to duplicate
  *
  * Return: Return success.
 **/
+
 char *_strdup(char *str)
 {
-char *strnew = NULL;
-unsigned int i;
-int n;
+int i;
+int size = _strlen(str) + 1;
+char *ar;
+char *_strdup = NULL;
+str = NULL;
 
 if (str == NULL)
 {
 return (NULL);
 }
+ar = malloc(size * sizeof(char));
 
-for (n = 0; str[n] != '\0'; n++)
-
-strnew = (char *)malloc(n + 1 * sizeof(char));
-if (strnew != NULL)
-{
-for (i = 0; str[i] != '\0'; i++)
-{
-strnew[i] = str[i];
-}
-}
-else
+if (ar == NULL)
 {
 return (NULL);
 }
-strnew[i] = '\0';
-return (strnew);
+else
+{
+for (i = 0; i < size; i++)
+{
+ar[i] = str[i];
+ar[i + 1] = '\0';
+}
+return (ar);
+}
 }
