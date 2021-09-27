0e2d27ee3ef605a8af737fad8ac7d180eb0cb981
AyshaMuss <ayshamuss@gmail.com>
2021-09-28 00:26:44 +0300

    0x0E structures typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,24 +14,24 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-  dog_t *newDog;
-  char *newName;
-  char *newOwner;
+dog_t *newDog;
+char *newName;
+char *newOwner;
 int size = 4;
 
-newDog = (dog_t *)malloc(sizeof(struct dog)*size);
+newDog = (dog_t *)malloc(sizeof(struct dog) * size);
 if (newDog == NULL)
 {
 free(newDog);
 return (NULL);
 }
-newName = (char *)malloc(sizeof((name) + 1)*size);
+newName = (char *)malloc(sizeof((name) + 1) * size);
 if (newName == NULL)
 {
 free(newDog);
 return (NULL);
 }
-newOwner = (char *)malloc(sizeof((owner) + 1)*size);
+newOwner = (char *)malloc(sizeof((owner) + 1) * size);
 if (newOwner == NULL)
 {
 free(newDog);
