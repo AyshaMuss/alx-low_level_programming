650d4d1b12722db2b3661a90241cf0bc4feaa842
AyshaMuss <ayshamuss@gmail.com>
2021-09-29 19:15:03 +0300

    0x0F function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: is pointer
+ * Return: Nothing.
+**/
+void print_name(char *name, void (*f)(char *))
+{
+int i;
+i = 0;
+if (name)
+f(&name[i]);
+return;
+}
