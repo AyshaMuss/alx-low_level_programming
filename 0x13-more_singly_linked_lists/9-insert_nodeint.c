fb3b9a0c5d33f6e85daa91a13d27fa5c6bf4cf61
AyshaMuss <Ayshamuss@gmail.com>
2021-10-12 15:36:37 +0300

    0x13 more singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,47 +1,40 @@
 #include "lists.h"
 #include <stdlib.h>
-
 /**
- * insert_nodeint_at_index - inserts a listint_t node at index in list
+ * insert_nodeint_at_index - insert a node at index
+ * @head: head of the list
+ * @index: location to insert node
+ * @n: value of the inserted node
  *
- * @head: head of list
- * @idx: index to add node at, starting at 0
- * @n: value of node to add
- *
- * Return: new node address if success, NULL otherwise
- */
+ * Return: pointer to head of list
+**/
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 {
-listint_t *ptr, *prev = NULL;
-int ct = idx;
+listint_t *current, *new;
 
-if (head == NULL)
+current = *head;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
 return (NULL);
-
-for (ptr = *head; ct && ptr != NULL; ct--, ptr = ptr->next)
-prev = ptr;
-if (ct)
+new->n = n;
+if (index == 0)
+{
+new->next = current;
+*head = new;
+return (*head);
+}
+while (index > 1)
+{
+current = current->next;
+index--;
+if (!current)
+{
+free(new);
 return (NULL);
-
-ptr = malloc(sizeof(listint_t));
-if (ptr == NULL)
-return (NULL);
-
-if (prev != NULL)
-{
-ptr->next = prev->next;
-prev->next = ptr;
 }
-else
-{
-ptr->next = NULL;
 }
-if (*head == NULL || idx == 0)
-{
-ptr->next = *head;
-*head = ptr;
-}
-ptr->n = n;
-return (ptr);
+new->next = current->next;
+current->next = new;
+return (new);
 }
