099a51a3dba26a7d4548237155b9292f4150d6af
AyshaMuss <Ayshamuss@gmail.com>
2021-10-12 15:48:09 +0300

    0x13 more singly linked lists

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - define function
+ * @head: is a pointer to constant listint_t
+ *
+ * Return: what does it return?
+**/
+
+size_t print_listint_safe(const listint_t *head)
+{
+head++;
+return (0);
+}
