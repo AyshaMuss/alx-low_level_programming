6a2aa4133d2c53e323335f41f1e62e3e444dbfc4
AyshaMuss <Ayshamuss@gmail.com>
2021-10-13 12:44:38 +0300

    0x13 more singly linked lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,6 +2,40 @@
 #include <stdio.h>
 #include "lists.h"
 
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Print numbers
+ * @separator: char separating numbers
+ * @n: number of iterations
+ * return: Returns an int
+**/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i, val;
+va_list ls;
+
+va_start(ls, n);
+
+for (i = 0; i < n; i++)
+{
+val = va_arg(ls, int);
+print_numbers("%d", val);
+if (separator)
+{
+continue;
+if (i != (n - 1))
+printf("%s", separator);
+}
+}
+printf("\n");
+va_end(ls);
+return;
+}
+
 /**
  *print_listint_safe - prints a listint list
  *@head:pointer to head
@@ -9,15 +43,31 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-unsigned int i = 0;
+const listint_t **list = NULL;
+size_t i, num = 0;
 
 if (!head)
 exit(98);
+
 while (head)
 {
+for (i = 0; i < num; i++)
+{
+
+if (head == list[i])
+{
+printf("[%p] %d", (void *)head, head->n)
+;
+
+free(list);
+return (num);
+}
+}
+num++;
 printf("[%p] %d", (void *)head, head->n);
-i++;
 head = head->next;
 }
-return (i);
+
+free(list);
+return (num);
 }
