ab5b40fa216475d167602db3733c5187d055e8c2
AyshaMuss <ayshamuss@gmail.com>
2021-10-15 09:50:39 +0300

    0x14 bit manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,7 +2,7 @@
 
 /**
 * clear_bit - is a function set bit to 0 at given index.
-* @*n: is unsigned long int pointer
+* @n: is unsigned long int
 * @index: is an unsigned int
 *
 * Return: return 1 when bit is 1 else 0.
